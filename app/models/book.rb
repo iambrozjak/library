b09e3dd78f7491ed14ffb33737d1c8a0b66e2319
iambrozjak <ivanochkavasyljuk@gmail.com>
2023-10-13 23:33:15 +0300

    Validates has been added

--- app/models/book.rb
+++ app/models/book.rb
@@ -1,2 +1,5 @@
 class Book < ApplicationRecord
+    validates :title, :author, presence: true
+    validates :isbn, numericality: { only_integer: true }, length: { is: 13 }
+    validates :description, length: { maximum: 100 }
 end
