b6b900a32c73915511a9718b71bb854021b0e63f
iambrozjak <ivanochkavasyljuk@gmail.com>
2023-10-19 22:22:58 +0300

    Refactored book_model and db directory

--- app/models/book.rb
+++ app/models/book.rb
@@ -2,4 +2,7 @@ class Book < ApplicationRecord
     validates :title, :author, presence: true
     validates :isbn, numericality: { only_integer: true }, length: { is: 13 }
     validates :description, length: { maximum: 100 }
+
+    scope :ordered, -> { order(title: :asc) }
 end
+
