471153f9bdf3aace2d3127d56e5ad48d534461d5
iambrozjak <ivanochkavasyljuk@gmail.com>
2023-10-23 00:28:37 +0300

    Refactored view and  db directory

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,10 +1,1 @@
-require "faker"
-
-(1..50).each do |id|
-    Book.create(
-        title: Faker::Book.title,
-        author: Faker::Book.author,
-        isbn: Faker::Number.number(digits: 13),
-        description: Faker::Book.genre 
-    )
-end 
+FactoryBot.create_list(:book, 50)
