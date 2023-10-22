7e1aca9fa48d1c9f90638fb539a33d4f9b0ba6c3
iambrozjak <ivanochkavasyljuk@gmail.com>
2023-10-22 23:40:36 +0300

    Refactored books_controller directory

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -1,11 +1,11 @@
 class BooksController < ApplicationController
 
   def index
-    @books = Book.order(:id)
+    @books = collection
   end
 
   def show
-    @book = Book.find(params[:id])
+    @book = resourse
   end
 
   def new
@@ -13,7 +13,7 @@ class BooksController < ApplicationController
   end
 
   def edit
-    @book = Book.find(params[:id])
+    @book = resourse
   end
 
   def create
@@ -23,28 +23,38 @@ class BooksController < ApplicationController
       redirect_to book_path(@book), notice: "Book was successfully created."
     else
       render :new, status: :unprocessable_entity
-    end
+    end    
   end
 
   def update
-    @book = Book.find(params[:id])
+    @book = resourse
+
     if @book.update(book_params)
-      redirect_to book_path(@book), notice: "Book was successfully updated."
+      redirect_to book_path(@book), notice: "Book was successfully updated.", status: :see_other
     else
       render :edit, status: :unprocessable_entity
     end
   end
 
   def destroy
-    @book = Book.find(params[:id])
+    @book = resourse
+
     @book.destroy
-    redirect_to books_path, notice: 'Book was successfully destroyed.',
-    status: :see_other
+
+    redirect_to book_path(@book), notice: "Book was successfully destroyed.", status: :see_other
   end
 
   private
 
   def book_params
       params.require(:book).permit(:title, :author, :isbn, :description)
+  end
+
+  def collection
+    Book.ordered
+  end
+
+  def resourse
+    collection.find(params[:id])
   end
 end
