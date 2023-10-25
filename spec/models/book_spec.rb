d86b84f819ef64432dce1469abbbc5233765a14a
iambrozjak <ivanochkavasyljuk@gmail.com>
2023-10-25 23:28:55 +0300

    Fixed tests

--- spec/models/book_spec.rb
+++ spec/models/book_spec.rb
@@ -8,35 +8,13 @@ RSpec.describe Book, type: :model do
     expect(book.valid?).to eq true
   end
 
-
-it  "should create  book" do
-  book =  Book.create!(title:'Look to Windward', author:'Benjamin Wolf', isbn:'9372731109575', description:'Fantasy')
-  expect(book.title).to eq ('Look to Windward')
-end  
-
-it  "should update title " do
-  book =  Book.create!(title:'Look to Windward', author:'Benjamin Wolf', isbn:'9372731109575', description:'Fantasy')
-  expect(book.title).to eq ('Look to Windward')
-  book.title = 'New book'
-  book.save
-  expect(book.title).to eq ('New book')
-end  
-
-
-it  "should delete book" do
-  book = Book.create!(title:'Look to Windward', author:'Benjamin Wolf', isbn:'9372731109575', description:'Fantasy')
-  expect(book.title).to eq ('Look to Windward')
-  book.destroy
-  expect(Book.find_by(title: 'Look to Windward')).to be_nil
-end
-
-describe 'validations' do
-  it { is_expected.to validate_presence_of(:title) }
-  it { is_expected.to validate_presence_of(:author) }
-  it { is_expected.to validate_numericality_of(:isbn) }
-  it { is_expected.to validate_length_of( :description).is_at_most(100) }
-  it { is_expected.to validate_length_of( :isbn).is_equal_to(13) }
-end
+ describe 'validations' do
+   it { is_expected.to validate_presence_of(:title) }
+   it { is_expected.to validate_presence_of(:author) }
+   it { is_expected.to validate_numericality_of(:isbn) }
+   it { is_expected.to validate_length_of( :description).is_at_most(100) }
+   it { is_expected.to validate_length_of( :isbn).is_equal_to(13) }
+  end
 end
 
 
--- spec/requests/books_spec.rb
+++ spec/requests/books_spec.rb
@@ -91,5 +91,26 @@ RSpec.describe BooksController, type: :request do
       end.to change(Book, :count).by(-1)
     end
    end  
+
+it  "should create  book" do
+  book =  Book.create!(title:'Look to Windward', author:'Benjamin Wolf', isbn:'9372731109575', description:'Fantasy')
+  expect(book.title).to eq ('Look to Windward')
+end  
+
+it  "should update title " do
+  book =  Book.create!(title:'Look to Windward', author:'Benjamin Wolf', isbn:'9372731109575', description:'Fantasy')
+  expect(book.title).to eq ('Look to Windward')
+  book.title = 'New book'
+  book.save
+  expect(book.title).to eq ('New book')
+end  
+
+
+it  "should delete book" do
+  book = Book.create!(title:'Look to Windward', author:'Benjamin Wolf', isbn:'9372731109575', description:'Fantasy')
+  expect(book.title).to eq ('Look to Windward')
+  book.destroy
+  expect(Book.find_by(title: 'Look to Windward')).to be_nil
+end
 end
 
