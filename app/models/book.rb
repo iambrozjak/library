9498c2564f383f0da66b4735b2a2012e8daaffd4
iambrozjak <ivanochkavasyljuk@gmail.com>
2023-11-18 22:21:17 +0200

    Added cover and content parameters

--- app/models/book.rb
+++ app/models/book.rb
@@ -1,4 +1,7 @@
 class Book < ApplicationRecord
+  has_one_attached :cover
+  has_one_attached :content
+
   validates :title, :author, presence: true
   validates :isbn, presence: true, uniqueness: true,  numericality: { only_integer: true }, length: { is: 13 }
   validates :description, length: { maximum: 100 }
