43184a91e9e234c4900e8d1dad27d491e164b53c
iambrozjak <ivanochkavasyljuk@gmail.com>
2023-11-19 00:47:02 +0200

    Added tests for cover and content

--- spec/factories/books.rb
+++ spec/factories/books.rb
@@ -13,4 +13,12 @@ FactoryBot.define do
   trait :new_title do
     title { "NewTitle" }
   end
+
+  trait :with_cover do
+    cover { Rack::Test::UploadedFile.new("spec/fixtures/cover_example.jpg", "image/jpeg") }
+  end
+
+  trait :with_content do
+    content { Rack::Test::UploadedFile.new("spec/fixtures/content_example.pdf", "application/pdf") }
+  end
 end
--- spec/models/book_spec.rb
+++ spec/models/book_spec.rb
@@ -2,6 +2,8 @@ require 'rails_helper'
 
 RSpec.describe Book, type: :model do
   let(:book) { create(:book) }
+  let(:book) { create(:book, :with_cover, :with_content) }
+
 
   it "is persisted to the database" do
     expect(book).to be_persisted
@@ -13,5 +15,8 @@ RSpec.describe Book, type: :model do
     it { is_expected.to validate_numericality_of(:isbn) }
     it { is_expected.to validate_length_of( :description).is_at_most(100) }
     it { is_expected.to validate_length_of( :isbn).is_equal_to(13) }
-   end
+
+    it { is_expected.to have_one_attached (:cover) }
+    it { is_expected.to have_one_attached (:content) }
+  end
 end
