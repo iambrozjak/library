1e314f12dd9601cfff852d5e3f663f9cf37e556a
iambrozjak <ivanochkavasyljuk@gmail.com>
2023-11-26 12:40:49 +0200

    Added update_index

--- app/models/book.rb
+++ app/models/book.rb
@@ -1,4 +1,6 @@
 class Book < ApplicationRecord
+  update_index('books') { self }
+
   validates :title, :author, presence: true
   validates :isbn, presence: true, uniqueness: true,  numericality: { only_integer: true }, length: { is: 13 }
   validates :description, length: { maximum: 100 }
